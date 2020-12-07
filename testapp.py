6f5b835b8e550e178c26aec8f263b5d27d7eee3f
RussellMcGrath <mcgrath.russell@gmail.com>
2020-12-07 13:37:15 -0500

    switched to sqlite db

--- testapp.py
+++ testapp.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "postgresql://postgres:postgres@localhost:5432/firedata_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite" #"postgresql://postgres:postgres@localhost:5432/firedata_db"
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
